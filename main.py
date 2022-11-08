bf6e26979959fbd508228fc13e369dd9efe444cd
Cosy-sweater <vlad.belov37@yandex.ru>
2022-11-08 22:18:01 +0300

    better than previous one image less

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pygame_menu
 from pygame.locals import *
 
 pygame.init()
+pygame.font.init()
 
 fps = 60
 fpsClock = pygame.time.Clock()
@@ -18,17 +19,18 @@ settings = {
     "hitbox": False,
     "del": 1500
 }
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
 
 
 class Bird(pygame.sprite.Sprite):
-    def __init__(self, settings):
+    def __init__(self):
         super().__init__()
         self.image = pygame.image.load("img/bird.png")
         self.image = pygame.transform.rotozoom(self.image, 0, 0.1)
         self.rect = self.image.get_rect()
         self.rect.width = 68
         self.surface = pygame.Surface((self.rect.width, self.rect.height))
-        self.surface.fill((255, 0, 0))
+        self.surface.fill((255, 227, 0))
         self.rect.center = (120, 50)
         self.y_vel = 0
 
@@ -60,13 +62,17 @@ class Pipes(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.speed = 5
-        self.pipe_img1 = pygame.image.load("img/pipe_top.png")
-        self.pipe_img2 = pygame.image.load("img/pipe_bottom.png")
-
+        self.pipe_img1 = pygame.image.load("img/pipe.png")
         self.pipe_img1 = pygame.transform.rotozoom(self.pipe_img1, 0, 0.4)
-        self.pipe_img2 = pygame.transform.rotozoom(self.pipe_img2, 0, 0.4)
+
+        self.rect1 = self.pipe_img1.get_rect()
+        self.pipe_img2 = pygame.transform.flip(self.pipe_img1, False, True)
+
         self.rect1 = self.pipe_img1.get_rect()
         self.rect2 = self.pipe_img2.get_rect()
+
+        self.surf = pygame.Surface((self.rect1.width, self.rect1.height))
+        self.surf.fill((0, 255, 0))
 
         self.rect1.center = (740, 50)
         self.rect2.center = (740, 50)
@@ -76,6 +82,9 @@ class Pipes(pygame.sprite.Sprite):
         self.rect2.top = self.cord + settings["dist"]
 
     def draw(self):
+        if settings["hitbox"]:
+            screen.blit(self.surf, self.rect1)
+            screen.blit(self.surf, self.rect2)
         screen.blit(self.pipe_img1, self.rect1)
         screen.blit(self.pipe_img2, self.rect2)
 
@@ -89,16 +98,16 @@ class Pipes(pygame.sprite.Sprite):
 def start_game():
     t = time.time()
     game_loop, d = True, False
-    bird = Bird(settings)
+    bird = Bird()
     pipes = []
-    score = 0
+    score = -1
 
     pipe_event = pygame.USEREVENT + 1
     pygame.time.set_timer(pipe_event, settings["del"])
 
     # Game loop.
     while game_loop:
-        screen.fill((107, 180, 214))
+        screen.fill((112, 195, 244))
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -127,6 +136,11 @@ def start_game():
         # Draw.
         bird.draw()
         [i.draw() for i in pipes]
+        if score >= 0:
+            text_surface = my_font.render(str(score), False, (0, 0, 0))
+        else:
+            text_surface = my_font.render("0", False, (0, 0, 0))
+        screen.blit(text_surface, (0, 0))
 
         pygame.display.flip()
         fpsClock.tick(fps)
@@ -163,7 +177,7 @@ menu.add.range_slider('Зазор между трубами', default=settings["
 menu.add.range_slider('Расстояние между трубами', default=settings["del"], range_values=[800, 2000], increment=10,
                       value_format=lambda n: str(int(n)), onchange=set_distance)
 
-menu.add.selector('Хитбоксы:', [('да', True), ('нет', False)], onchange=set_difficulty, default=settings["hitbox"])
+menu.add.selector('Хитбоксы:', [('да', True), ('нет', False)], onchange=set_difficulty, default=1)
 menu.add.button("Сбросить настройки", reset_settings)
 menu.add.button('Играть', start_game)
 menu.mainloop(screen)
